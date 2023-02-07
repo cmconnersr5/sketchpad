31488c6172f4c9a87ba2fab84b107b8925db2c8e
cmconnersr5 <121987232+cmconnersr5@users.noreply.github.com>
2023-02-07 08:40:47 -0500

    Add basic styles and sketchpad

---
+++ index.js
@@ -0,0 +1,11 @@
+function createPad(size){
+let sketchpad = document.getElementById('sketchpad');
+sketchpad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+sketchpad.style.gridTemplateRows = `repeat(${size}, 1 fr)`;
+
+for (i=0; i<(size*size); i++){
+    let box = document.createElement('div');
+    sketchpad.insertAdjacentElement('beforeend', box);
+}
+
+} createPad(10);
