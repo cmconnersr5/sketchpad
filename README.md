9fdd64e16202d4267fb60c752313196533f77088
cmconnersr5 <121987232+cmconnersr5@users.noreply.github.com>
2023-02-07 11:50:42 -0500

    Add button functions

--- README.md
+++ README.md
@@ -2,11 +2,5 @@ sketchpad
 Odin Project Etch-a-Sketch
 
 //ToDo's
-// create 16x16 grid of divs
-    //use flex grid (grid-template-columns )
-// setup "hover" effect
 // add popup asking for grid size
-// maximum input 100
-// remove and replace after grid size entered
-// rgb option
-// 10% black fill option 
+
--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+let color = 'black';
+
 function createPad(size){
+    if (size>=2 && size <=100){
 let sketchpad = document.getElementById('sketchpad');
+let pixels = sketchpad.querySelectorAll('div');
+pixels.forEach((div) => div.remove())
 sketchpad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 sketchpad.style.gridTemplateRows = `repeat(${size}, 1 fr)`;
 
 for (i=0; i<(size*size); i++){
     let box = document.createElement('div');
     sketchpad.insertAdjacentElement('beforeend', box);
-}
+    box.addEventListener('mouseover', colorChange)
+}} else {
+    alert('Size must be greater than 2 and no more than 100')
+}};
+
+function colorChange(){
+    if (color === 'rainbow'){
+        this.style.backgroundColor = `hsl(${Math.random()*350}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    };
 }
 
+function colorSet(x){
+    color = x;
+};
